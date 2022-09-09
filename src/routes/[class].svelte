e3f6d25cc00a457708d271f3f6e2c9c2d2dc8940
fiskgrodan <jam@Johans-MacBook-Pro.local>
2022-09-09 12:32:57 +0200

    feat: use capital lettered class names

--- src/routes/[class].svelte
+++ src/routes/[class].svelte
@@ -1,23 +1,26 @@
 <script>
-	import { page } from '$app/stores';
+  import { page } from "$app/stores";
+
+  $: classname =
+    $page.params.class[0].toUpperCase() + $page.params.class.slice(1);
 </script>
 
 <svelte:head>
-	<title>Uthplan {$page.params.class}</title>
+  <title>Uthplan {classname}</title>
 </svelte:head>
 
 <main>
-	<div class="classname">{$page.params.class}</div>
+  <div class="classname">{classname}</div>
 </main>
 
 <style>
-	main {
-		width: 1000px;
-		margin: auto;
-		padding-top: 20px;
-	}
+  main {
+    width: 1000px;
+    margin: auto;
+    padding-top: 20px;
+  }
 
-	.classname {
-		text-transform: capitalize;
-	}
+  .classname {
+    text-transform: capitalize;
+  }
 </style>
